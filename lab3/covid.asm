6f7b6fb472587d93e64d35d41b74ec476ceb1ab0
mochisvelas <velasquezmochis@gmail.com>
2020-09-23 21:25:10 -0600

    Fixed dumb bug

--- lab3/covid.asm
+++ lab3/covid.asm
@@ -8,16 +8,19 @@
 	yll DB 'Alerta: Amarilla$'
 	grn DB 'Alerta: Verde$'
 
-	tNum DB ? 		;Tests number
-	ptNum DB ? 		;Positiv tests number
+	tNum DW ? 		;Tests number
+	ptNum DW ? 		;Positiv tests number
 
 	cnt DB 00h
+	tmp DW ?
 
 .stack
 .code
 program:
 	MOV AX,@DATA
 	MOV DS,AX
+	XOR AX,AX
+	XOR BX,BX
 
 	;Ask for tests quantity
 	MOV DX,offset tInput
@@ -33,17 +36,18 @@ program:
 	CMP AL,0Dh 		;Check if is an enter
 	JE nextnum 	 	;If yes, start program
 
-	CMP cnt,03h 		;Check if 3 digits have been inserted
+	CMP cnt,02h 		;Check if 3 digits have been inserted
 	JE nextnum 	 	;If yes, start program
 
 	SUB AL,30h 		;Convert to real number
 	XOR AH,AH 		;Clear AH
 
 	;Store the tens and hundreds if existent
-	SLL BX,01h 		;Multiply itself by 2
-	MOV CX,BX
-	SLL BX,02h 		;Multiply itself by 4, so by 8 in total
-	ADD BX,CX 		;By 10 in total
+	SHL BX,01h 		;Multiply itself by 2
+	MOV tmp,BX
+	MOV CL,02h
+	SHL BX,CL 		;Multiply itself by 4, so by 8 in total
+	ADD BX,tmp 		;By 10 in total
 	ADD BX,AX
 	MOV tNum,BX 		;Store in variable
 	ADD cnt,01h 		;Add 1 to cont
@@ -54,19 +58,20 @@ program:
 	
 	nextnum:
 
-	XOR BX,BX
-	XOR AX,AX
-	MOV cnt,00h
-
 	;New line
 	MOV DL,0AH
 	MOV AH,02h
 	INT 21h
 
+
 	;Ask for positive tests quantity
 	MOV DX,offset ptInput
 	MOV AH,09h
 	INT 21h
+
+	XOR AX,AX
+	XOR BX,BX
+	MOV cnt,00h
 
 	;Loop to store all digits
 	ptloop:
@@ -77,20 +82,23 @@ program:
 	CMP AL,0Dh 		;Check if is an enter
 	JE startprogram 	;If yes, start program
 
-	CMP cnt,03h 		;Check if 3 digits have been inserted
-	JE startprogram 	;If yes, start program
+	CMP cnt,02h 		;Check if 3 digits have been inserted
+	JE startprogram 	 	;If yes, start program
 
 	SUB AL,30h 		;Convert to real number
 	XOR AH,AH 		;Clear AH
 
 	;Store the tens and hundreds if existent
-	SLL BX,01h 		;Multiply itself by 2
-	MOV CX,BX
-	SLL BX,02h 		;Multiply itself by 4, so by 8 in total
-	ADD BX,CX 		;By 10 in total
+	SHL BX,01h 		;Multiply itself by 2
+	MOV tmp,BX
+	MOV CL,02h
+	SHL BX,CL 		;Multiply itself by 4, so by 8 in total
+	ADD BX,tmp 		;By 10 in total
 	ADD BX,AX
 	MOV ptNum,BX 		;Store in variable
 	ADD cnt,01h 		;Add 1 to cont
+	
+	JMP ptloop
 
 ;-----------------------------------------------------------------------
 	startprogram:
